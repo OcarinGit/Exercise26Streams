5d114b72a6d175bdd01d7d92d0cdd87defa164fc
OcarinGit <oscarsantanaalvarez@gmail.com>
2019-10-24 12:23:12 -0500

    FileWriter

---
+++ src/com/exercise26streams/app/WriterApp.java
@@ -0,0 +1,54 @@
+package com.exercise26streams.app;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.util.Scanner;
+
+public class WriterApp
+{
+	public static void main(String[] args) 
+	{
+		//Constant declaration
+		final int LIMIT = 24;
+		File myFile = null;
+		FileWriter myWriter = null;
+		BufferedWriter myBuffer = null;
+		Scanner input = new Scanner(System.in);
+		int table=0;
+		
+		try
+		{
+			myFile = new File("d:\\a\\b.txt");
+			myWriter = new FileWriter(myFile);
+			myBuffer = new BufferedWriter(myWriter);
+			
+			System.out.println("Input the multiplication table:");
+			table=input.nextInt();
+			for(int i=0;i<=LIMIT;i++)
+			{
+				System.out.println(String.format("%d X %d = %d", table,i,table*i));
+				myBuffer.write(String.format("%d X %d = %d \n", table,i,table*i));
+			}
+			
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try {
+				myBuffer.close();
+				myWriter = null;
+				myFile = null;
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
